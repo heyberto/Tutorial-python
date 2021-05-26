9aaa31a983a644226be808ba4fd96a48c30b3f61
heyberto <albertuxpro18@gmail.com>
2021-05-26 17:04:04 +0200

    Subiendo los archivos de python

---
+++ Tutorial3.py
@@ -0,0 +1,25 @@
+
+#mates, redondear
+
+import math
+pi = 3.5
+x = 1
+y = 2
+z = 3
+
+#print(round(pi))
+#print(math.ceil(pi))
+#print(math.floor(pi))
+
+#VALOR ABSOLUTO
+#print(abs(pi))
+
+#elevar el numero
+#print(pow(pi,2))
+
+#raiz cuadrada
+#print(math.sqrt(pi))
+
+#MAYOR Y MENOR NUMERO
+#print(max(x,y,z))
+#print(min(x,y,z))
---
+++ Tutorial4.py
@@ -0,0 +1,8 @@
+#string slicing
+
+nombre = 'berto'
+
+first_name = nombre[0:3]
+last_name = nombre[3:]
+print(first_name)
+print(last_name)
